5144ff8e0ed36c77b9307e66cc2dbdfdd52fe4c5
Jatin_goibibo <jatinsethi95@yahoo.com>
2026-01-12 23:10:30 +0530

    Updated logic for github and local browser initialization

--- test_base/base.py
+++ test_base/base.py
@@ -1,5 +1,6 @@
 import pytest
 import yaml
+import os
 from playwright.sync_api import sync_playwright
 
 @pytest.fixture(scope="function")
@@ -7,7 +8,12 @@ def page():
     config = yaml.safe_load(open("config/config.yaml"))
 
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False, args=["--start-maximized"])
+        headless_mode = True if os.getenv("CI") else False
+
+        browser = p.chromium.launch(
+            headless=headless_mode,
+            args=["--start-maximized"] if not headless_mode else []
+        )
         context = browser.new_context(no_viewport=True)
         page = context.new_page()
         page.goto(config["base_url"])
